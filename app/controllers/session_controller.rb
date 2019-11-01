4f21aed0736e0e05e4d31eb9bdb68b333fb8a4df
sbouras753 <55755809+sbouras753@users.noreply.github.com>
2019-11-01 08:30:43 +0100

    Update session_controller.rb

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,10 +6,9 @@ class SessionController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Email ou mot de passe invalide'
       render 'new'
     end
   end
